f42bff16b60be20a030a75a1acf67cc1cef87411
Chris Reynolds <chris@jazzsequence.com>
2024-12-10 19:59:55 -0700

    remove unused/old fields code

--- web/private/scripts/slack-notification.php
+++ web/private/scripts/slack-notification.php
@@ -4,124 +4,15 @@
  * Description: Send a notification to a Slack channel when code is deployed to Pantheon.
  */
 
-// "Important" constants.
 $pantheon_yellow = '#FFDC28';
+$slack_channel = '#firehose';
 
-/** 
- * Default values for parameters - this will assume the channel you define the webhook for.
- * The full Slack Message API allows you to specify other channels and enhance the messagge further if you like: https://api.slack.com/docs/messages/builder.
- */
-$defaults = [
-	'always_show_text' => false,
-	'slack_channel' => '#firehose',
-];
-
-/**
- * Build an array of fields to be rendered with Slack Attachments as a table attachment-style formatting: https://api.slack.com/docs/attachments
- */
-$fields = [
-	[
-		'title' => 'Site',
-		'value' => $_ENV['PANTHEON_SITE_NAME'],
-		'short' => 'true',
-	],
-	[ // Render Environment name with link to site, <http://{ENV}-{SITENAME}.pantheon.io|{ENV}>.
-		'title' => 'Environment',
-		'value' => '<http://' . $_ENV['PANTHEON_ENVIRONMENT'] . '-' . $_ENV['PANTHEON_SITE_NAME'] . '.pantheonsite.io|' . $_ENV['PANTHEON_ENVIRONMENT'] . '>',
-		'short' => 'true',
-	],
-	[ // Render Name with link to Email from Commit message.
-		'title' => 'By',
-		'value' => $_POST['user_email'],
-		'short' => 'true',
-	],
-	[ // Render workflow phase that the message was sent.
-		'title' => 'Workflow',
-		'value' => ucfirst( $_POST['stage'] ) . ' ' . str_replace( '_', ' ', $_POST['wf_type'] ),
-		'short' => 'true',
-	],
-	[
-		'title' => 'View Dashboard',
-		'value' => '<https://dashboard.pantheon.io/sites/' . PANTHEON_SITE . '#' . PANTHEON_ENVIRONMENT . '/deploys|View Dashboard>',
-		'short' => 'true',
-	],
-];
-
-/**
- * Customize the message based on the workflow type.  Note that slack_notification.php must appear in your pantheon.yml for each workflow type you wish to send notifications on.
- */
 print("\n==== Debugging Workflow Type ====\n");
 print("wf_type: " . $_POST['wf_type'] . "\n");
-switch ( $_POST['wf_type'] ) {
-	case 'deploy':
-		// Find out what tag we are on and get the annotation.
-		$deploy_tag = `git describe --tags`;
-		$deploy_message = $_POST['deploy_message'];
 
-		// Prepare the slack payload as per: https://api.slack.com/incoming-webhooks.
-		$text = 'Deploy to the ' . $_ENV['PANTHEON_ENVIRONMENT'];
-		$text .= ' environment of ' . $_ENV['PANTHEON_SITE_NAME'] . ' by ' . $_POST['user_email'] . ' complete!';
-		$text .= ' <https://dashboard.pantheon.io/sites/' . PANTHEON_SITE . '#' . PANTHEON_ENVIRONMENT . '/deploys|View Dashboard>';
-		// Build an array of fields to be rendered with Slack Attachments as a table attachment-style formatting: https://api.slack.com/docs/attachments.
-		$fields[] = [
-			'title' => 'Details',
-			'value' => $text,
-			'short' => 'false',
-		];
-		$fields[] = [
-			'title' => 'Deploy Note',
-			'value' => $deploy_message,
-			'short' => 'false',
-		];  
-		break;
-
-	case 'sync_code':
-		$git_version = trim(`git --version`);
-		print("\n==== Debugging Git Availability ====\n");
-		print("Git Version: $git_version\n");
-
-		// Get the committer, hash, and message for the most recent commit.
-		$committer = `git log -1 --pretty=%cn`;
-		$email = `git log -1 --pretty=%ce`;
-		$message = `git log -1 --pretty=%B`;
-		$hash = `git log -1 --pretty=%h`;		
-		print("\n==== Debugging git log ====\n");
-		print("Committer: $committer\n");
-		print("Hash: $hash\n");
-		print("Message: $message\n");
-		
-
-		// Prepare the slack payload as per: https://api.slack.com/incoming-webhooks.
-		$text = 'Code sync to the ' . $_ENV['PANTHEON_ENVIRONMENT'] . ' environment of ' . $_ENV['PANTHEON_SITE_NAME'] . ' by ' . $_POST['user_email'] . "!\n";
-		$text .= 'Most recent commit: ' . rtrim( $hash ) . ' by ' . rtrim( $committer ) . ': ' . $message;
-		// Build an array of fields to be rendered with Slack Attachments as a table attachment-style formatting: https://api.slack.com/docs/attachments.
-		$fields = array_merge($fields, [
-			[
-				'title' => 'Commit',
-				'value' => rtrim( $hash ),
-				'short' => 'true',
-			],
-			[
-				'title' => 'Commit Message',
-				'value' => rtrim( $message ),
-				'short' => 'false',
-			],
-		]);
-		break;
-
-	case 'clear_cache':
-		$fields[] = [
-			'title' => 'Cleared caches',
-			'value' => 'Cleared caches on the ' . $_ENV['PANTHEON_ENVIRONMENT'] . ' environment of ' . $_ENV['PANTHEON_SITE_NAME'] . "!\n",
-			'short' => 'false',
-		];
-		break;
-
-	default:
-		$text = $_POST['qs_description'];
-		break;
-}
-
+/**
+ * Build an array of fields to be rendered with Slack Attachments as a table using markdown and block-based sections.
+ */
 $base_blocks = [
     [
         'type' => 'section',
@@ -148,6 +39,9 @@ $base_blocks = [
 
 $blocks = $base_blocks;
 
+/**
+ * Customize the message based on the workflow type.  Note that slack_notification.php must appear in your pantheon.yml for each workflow type you wish to send notifications on.
+ */
 switch ($_POST['wf_type']) {
     case 'deploy':
         $deploy_message = $_POST['deploy_message'];
@@ -219,7 +113,7 @@ $attachments = [
 	],
 ];
 
-_slack_notification( $defaults['slack_channel'], $attachments );
+_slack_notification( $slack_channel, $attachments );
 
 /**
  * Send a notification to slack
