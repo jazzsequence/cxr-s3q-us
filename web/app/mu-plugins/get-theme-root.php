373fb1670e25d40fc810395186bbbc07c67764a0
Chris Reynolds <chris@jazzsequence.com>
2025-06-20 14:13:37 -0600

    fix linting

--- web/app/mu-plugins/get-theme-root.php
+++ web/app/mu-plugins/get-theme-root.php
@@ -4,7 +4,7 @@
  */
 
 if ( file_exists( ABSPATH . '/vendor/autoload.php' ) ) {
-	require_once dirname(__DIR__) . '/vendor/autoload.php';
+	require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 } else {
 	wp_die( 'Autoloader not found' );
 }
