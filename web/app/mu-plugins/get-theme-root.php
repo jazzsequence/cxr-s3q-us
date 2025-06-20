3c75256b5b42c910a158a7d1957448170e761dbb
Chris Reynolds <chris@jazzsequence.com>
2025-06-20 10:01:17 -0600

    use the right filter hook

--- web/app/mu-plugins/get-theme-root.php
+++ web/app/mu-plugins/get-theme-root.php
@@ -3,7 +3,7 @@
  * Require theme.php if it's not already loaded to avoid get_theme_root not defined error.
  */
 
-add_action( 'mu_plugin_loaded', function () {
+add_action( 'muplugins_loaded', function () {
 	if ( ! function_exists( 'get_theme_root' ) ) {
 		require_once WPINC . '/theme.php';
 	}
