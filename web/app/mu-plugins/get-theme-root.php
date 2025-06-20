29d09bf037592d6877a2f55a269e359c5c8d431b
Chris Reynolds <chris@jazzsequence.com>
2025-06-20 10:04:29 -0600

    maybe we just do the require

--- web/app/mu-plugins/get-theme-root.php
+++ web/app/mu-plugins/get-theme-root.php
@@ -3,8 +3,6 @@
  * Require theme.php if it's not already loaded to avoid get_theme_root not defined error.
  */
 
-add_action( 'muplugins_loaded', function () {
-	if ( ! function_exists( 'get_theme_root' ) ) {
-		require_once WPINC . '/theme.php';
-	}
-} );
+if ( ! function_exists( 'get_theme_root' ) ) {
+	require_once WPINC . '/theme.php';
+}
